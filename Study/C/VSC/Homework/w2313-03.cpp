aa4a3b67a1367a4236327a615401a8a4e0e1e00b
STONE1623 <1623804610@qq.com>
2024-11-11 12:58:52 +0800

    1111

--- Study/C/VSC/Homework/w2313-03.cpp
+++ Study/C/VSC/Homework/w2313-03.cpp
@@ -8,32 +8,59 @@ int main()
     while(cin>>n)
     {
         const int N=n;
-        int a[N];
+        int a[N];int c[N];
+        int num[N]; //储存原数组下标
         for(int i=0;i<N;i++) cin>>a[i];
+        for(int q=0;q<N;q++) c[q]=a[q];
         for(int k=0;k<N;k++)
         {
-            int s=k;int t=0;
-            for(int b=k+1;b<n;b++)
+            int s=k;int t=0;int min1=a[k];int min2=a[k];
+            //if(k<N-1)
+            //{
+            for(int b=k+1;b<N;b++) //完成排序
             {
-                if(a[b]<a[k]) s=b;
+                if(a[b]<a[k])
+                {
+                    min1=a[b];
+                    
+                    if(min2<a[b]) //if上一个min值更小
+                    {
+                        min1=min2;  
+                    }
+                    else  //此a[b]更小
+                    {
+                        s=b;
+                        num[k]=b;
+                    }
+                    min2=min1;
+                }
             }
+            
             if(s!=k)
             {
                 t=a[s];
                 a[s]=a[k];
                 a[k]=t;
-
             }
-            for(int y=0;y<N;y++)
+            else
+            {
+                num[k]=k;
+            }
+            /*for(int y=0;y<N;y++)
             {
                 cout<<a[y]<<" ";
             }
             cout<<endl;
+            //if(k==N-1) break;*/
         }
-        for(int y=0;y<N;y++)
+        for(int y=N-1;y>=0;y--)
         {
             cout<<a[y]<<" ";
+            for(int r=0;r<N;r++)
+            {
+                if(c[r]==a[y]) cout<<r<<endl;
+            }
         }
-        cout<<1;
+        
     }
 }
---
+++ Study/C/VSC/Homework/w2313-04.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <climits>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    string a;int num = 0;int x=0;
+    while(cin>>a)
+    {
+        num=a.length();
+        const int N=num;
+        char b[N];
+        strcpy(b,a.c_str());
+        if(N%2==0) 
+        {
+            for(int q=0;q<=N/2;q++)
+            {
+                if(b[q]!=b[N-1-q])
+                {
+                    x=1;
+                    break;
+                } 
+            }
+        }
+        else
+        {
+            for(int q=0;q<(N-1)/2;q++)
+            {
+                if(b[q]!=b[N-1-q])
+                {
+                    x=1;
+                    break;
+                }
+            }
+        }
+        cout<<a<<endl;
+        if(x==1) cout<<"不是回文数"<<endl;
+        else cout<<"是回文数"<<endl;
+        //for(int i=0;i<N;i++) cout<<b[i]<<" ";
+    }
+}
