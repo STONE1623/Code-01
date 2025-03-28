35cb9d17b03fe0ad58fb493ff070e0866bef6d0c
Breeket <47135301+STONE1623@users.noreply.github.com>
2025-03-28 18:33:28 +0800

    0328

--- Study/C/VSC/Homework/2359-04.cpp
+++ Study/C/VSC/Homework/2359-04.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 class nauticalmile_kilometer
@@ -6,14 +7,25 @@ class nauticalmile_kilometer
 public:
     double kilometer;
     double meter;
-    double mile;
-    nauticalmile_kilometer(int a,int b):kilometer(a),meter(b),mile(0) {}
+    nauticalmile_kilometer() {}
+    nauticalmile_kilometer(int a,int b):kilometer(a),meter(b){}
     void print()
     {
-        cout<<mile<<"\n";
+        cout<<kilometer<<"\n"<<meter<<"\n";
     }
     operator double()
     {
-
+        double mile;
+        mile=(kilometer+0.001*meter)/1.852;
+        return mile;
     }
 };
+int main()
+{
+    int a, b;
+    while(cin>>a>>b)
+    {
+        nauticalmile_kilometer n1(a,b);
+        cout<<fixed<<setprecision(4)<<double(n1)<<endl;
+    }
+}
